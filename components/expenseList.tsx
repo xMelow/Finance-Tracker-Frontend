349090cc52462412b9cbc5d448be7d1c5ea853ac
Flor Stellamans <flor.stellamans@student.howest.be>
2025-05-04 11:00:46 +0200

    refactor expenseList code

--- components/expenseList.tsx
+++ components/expenseList.tsx
@@ -1,35 +1,34 @@
 import { Expense } from "../types/expense";
 import styles from '../styles/expenseList.module.css';
 import { Category } from "../types/category";
+import { JSX } from "react";
 
 interface ExpenseListProps {
-    expenses: Expense[],
-    categories: Category[]
+  expenses: Expense[];
+  categories: Category[];
 }
 
-const ExpenseList: React.FC<ExpenseListProps> = ({ expenses, categories }) => {
-    return ( 
-        <div className={styles.container}>
-            {expenses.length === 0 ? (
-                <p className={styles.noExpenses}>No expenses found.</p>
-            ) : (
-                <ul className={styles.list}>
-                {expenses.map((ex) => (
-                    <li key={ex.id} className={styles.item}>
-                    <article className={styles.row}>
-                        <h2>{ex.description}</h2>
-                        <p className={styles.category}>
-                            {categories.find(cat => cat.id === Number(ex.categoryId))?.name ?? "Unknown"}
-                        </p>
-                        <p className={styles.amountPositive}>€ {ex.amount}</p>
-                        <p>{ex.date.toString()}</p>
-                    </article>
-                    </li>
-                ))}
-                </ul>
-            )}
-        </div>
-    );
+export default function ExpenseList({ expenses, categories }: ExpenseListProps): JSX.Element {
+  return (
+    <div className={styles.container}>
+      {expenses.length === 0 ? (
+        <p className={styles.noExpenses}>No expenses found.</p>
+      ) : (
+        <ul className={styles.list}>
+          {expenses.map((ex) => (
+            <li key={ex.id} className={styles.item}>
+              <article className={styles.row}>
+                <h2>{ex.description}</h2>
+                <p className={styles.category}>
+                  {categories.find((cat) => cat.id === Number(ex.categoryId))?.name ?? "Unknown"}
+                </p>
+                <p className={styles.amountPositive}>€ {ex.amount}</p>
+                <p>{new Date(ex.date).toLocaleDateString()}</p> {/* Date formatting */}
+              </article>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
 }
-
-export default ExpenseList
