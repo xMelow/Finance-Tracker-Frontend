587fc2fac301ae60e49459b49da8aa74df73a6c7
Flor Stellamans <flor.stellamans@student.howest.be>
2025-05-15 10:07:04 +0200

    add different pages with header

--- components/main/header/header.tsx
+++ components/main/header/header.tsx
@@ -1,14 +1,15 @@
-
+import Link from 'next/link';
+import styles from './header.module.css';
 
 export default function Header() {
     return (
         <header>
-            <h1>Finna</h1>
-            <nav>
+            <h1 className={styles.title}>Finna</h1>
+            <nav className={styles.nav}>
                 <ul>
-                    <li>Expenses</li>
-                    <li>Charts</li>
-                    <li>Totals</li>
+                    <li><Link href="/expenses">Expenses</Link></li>
+                    <li><Link href="/charts">Charts</Link></li>
+                    <li><Link href="/totals">Totals</Link></li>
                 </ul>
             </nav>
         </header>
---
+++ pages/totals/index.tsx
@@ -0,0 +1,12 @@
+import Header from "../../components/main/header/header";
+
+
+export default function Totals() {
+    return (
+         <>
+            <Header></Header>
+            <h1>Totals coming soon...</h1>
+        </>
+        
+    )
+}
