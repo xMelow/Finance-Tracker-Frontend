aaa5ba7c4c6b4f1c2831d0ff75c07c0d0a7bda6f
Flor Stellamans <flor.stellamans@student.howest.be>
2025-05-15 09:51:04 +0200

    add header component

---
+++ components/main/header/header.tsx
@@ -0,0 +1,16 @@
+
+
+export default function Header() {
+    return (
+        <header>
+            <h1>Finna</h1>
+            <nav>
+                <ul>
+                    <li>Expenses</li>
+                    <li>Charts</li>
+                    <li>Totals</li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
