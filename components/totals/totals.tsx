e443b3be8437741f963e40bfd97c4e18efe06a57
Flor Stellamans <flor.stellamans@student.howest.be>
2025-05-21 13:57:29 +0200

    header styling

--- components/totals/totals.tsx
+++ components/totals/totals.tsx
@@ -1,21 +1,14 @@
-import { monthSpending } from "../../types/monthSpending.";
 
 
 interface TotalProps {
     totalSpending: number | undefined;
-    totalMonthSpending: monthSpending[];
 }
 
-export default function Totals({ totalSpending, totalMonthSpending }: TotalProps) {
+export default function Totals({ totalSpending }: TotalProps) {
     return (
         <div>
             <p><strong>Total Spending:</strong> €{totalSpending?.toFixed(2) ?? 'Loading...'}</p>
-            {totalMonthSpending.map((month) => (
-                <p key={month.month}>
-                    <span>Month: {month.month} — </span>
-                    <span>€{month.total}</span>
-                </p>
-            ))}
+            {/* <p><strong>Total Spending this month:</strong> €{totalSpendingMonth?.toFixed(2) ?? 'Loading...'}</p> */}
         </div>
     )
 }
